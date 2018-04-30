190374dfcc702860e3ccf7f2f2140ea30dddc6be
Jennifer Walker <dataqueen16@gmail.com>
2018-04-30 14:32:37 -0500

    Taking in string sentences for manipulation

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,23 +3,32 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 
-        int userInput;
-        Scanner ip = new Scanner(System.in);
+//        int userInput;
+//        Scanner ip = new Scanner(System.in);
+//
+//        System.out.print("Please enter a positive integer: ");
+//        userInput = ip.nextInt();
+//
+//        System.out.println("You entered " + userInput);
 
-        System.out.print("Please enter a positive integer: ");
-        userInput = ip.nextInt();
+//        String first, second, third;
+//        Scanner wordIp = new Scanner(System.in);
+//
+//        System.out.print("Please enter any 3 words - ");
+//        first = wordIp.next();
+//        second = wordIp.next();
+//        third = wordIp.next();
+//
+//        System.out.println(first);
+//        System.out.println(second);
+//        System.out.println(third);
 
-        System.out.println("You entered " + userInput);
+        String sentence;
+        Scanner sentenceIp = new Scanner(System.in);
 
-        String first, second, third;
+        System.out.println("Please enter a sentence - any sentence! -");
+        sentence = sentenceIp.next();
 
-        Scanner wordIp = new Scanner(System.in);
-        first = wordIp.next();
-        second = wordIp.next();
-        third = wordIp.next();
-
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
+        System.out.println("Your sentence was.... " + sentence);
     }
 }
