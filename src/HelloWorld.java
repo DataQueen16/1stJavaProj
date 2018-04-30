b94629edc738162f8e4174b3a59352b8ae48788a
Jennifer Walker <dataqueen16@gmail.com>
2018-04-30 13:28:07 -0500

    Lesson 2 Java: variables and printing to the console

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, ");
-        System.out.print("World!");
+        System.out.println("Hello, World!");
+
+        int myFav = 40;
+        System.out.println(myFav);
+
+        String myString = 'A';
+        System.out.println(myString);
     }
 }
