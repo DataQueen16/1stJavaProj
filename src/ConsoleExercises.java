a1bc3a49326138826e99a10912718c1fb0785764
Jennifer Walker <dataqueen16@gmail.com>
2018-04-30 16:50:37 -0500

    while loop starting at 5, ends at 15

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,35 +3,39 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
 
-//        int userInput;
-//        Scanner ip = new Scanner(System.in);
-//
-//        System.out.print("Please enter a positive integer: ");
-//        userInput = ip.nextInt();
-//
-//        System.out.println("You entered " + userInput);
+        //Entering a positive integer
+        int userInput;
+        Scanner ip = new Scanner(System.in);
 
-//        String first, second, third;
-//        Scanner wordIp = new Scanner(System.in);
-//
-//        System.out.print("Please enter any 3 words - ");
-//        first = wordIp.next();
-//        second = wordIp.next();
-//        third = wordIp.next();
-//
-//        System.out.println(first);
-//        System.out.println(second);
-//        System.out.println(third);
+        System.out.print("Please enter a positive integer: ");
+        userInput = ip.nextInt();
 
-//        String sentence;
-//        Scanner sentenceIp = new Scanner(System.in);
-//
-//        System.out.println("Please enter a sentence - any sentence! -");
-//        sentence = sentenceIp.nextLine();
-//
-//        System.out.println("Your sentence was.... " + sentence);
+        System.out.println("You entered " + userInput);
 
-        float width,
+        //Entering any 3 words
+        String first, second, third;
+        Scanner wordIp = new Scanner(System.in);
+
+        System.out.print("Please enter any 3 words - ");
+        first = wordIp.next();
+        second = wordIp.next();
+        third = wordIp.next();
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+        //Entering a sentence
+        String sentence;
+        Scanner sentenceIp = new Scanner(System.in);
+
+        System.out.println("Please enter a sentence - any sentence! -");
+        sentence = sentenceIp.nextLine();
+
+        System.out.println("Your sentence was.... " + sentence);
+
+        //Calculating the area, perimeter & volume of the Codeup Classroom
+        double width,
                 length,
                 area,
                 height,
@@ -41,13 +45,13 @@ public class ConsoleExercises {
         Scanner areaIp = new Scanner(System.in);
 
         System.out.println("Please enter the width of the Codeup classroom: ");
-        width = areaIp.nextFloat();
+        width = areaIp.nextDouble();
 
         System.out.println("Now enter the length of the classroom: ");
-        length = areaIp.nextFloat();
+        length = areaIp.nextDouble();
 
         System.out.println("Now enter the height of the classroom: ");
-        height = areaIp.nextFloat();
+        height = areaIp.nextDouble();
 
         area = length * width;
         perimeter = (2 * length) + (2 * width);
