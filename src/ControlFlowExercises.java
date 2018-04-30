6c8f5845a703e181945623dca92f64da1264c3fd
Jennifer Walker <dataqueen16@gmail.com>
2018-04-30 17:42:25 -0500

    created a for loop incrementing by 5 until 15 instead of the while/do while loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,19 +1,16 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
-//        Do While
+//      While
 //
-//      Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
-//      Alter your loop to count backwards by 5's from 100 to -10.
-//      Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+//      Create an integer variable i with a value of 5.
+//      Create a while loop that runs so long as i is less than or equal to 15
+//      Each loop iteration, output the current value of i, then increment i by one.
+//      Your output should look like this:
 //
-//      2
-//      4
-//      16
-//      256
-//      65536
+//5 6 7 8 9 10 11 12 13 14 15
 
-        for (int i = 2; i < 1000000; i *= i) {
+        for (int i = 5; i <= 15; i++) {
             System.out.println(i);
         }
     }
