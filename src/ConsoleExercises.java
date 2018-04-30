5fc78766e79ade3d73b331fd2f579476e8ca25bb
Jennifer Walker <dataqueen16@gmail.com>
2018-04-30 14:50:50 -0500

    completed Java exercises and bonus portions. Changed variable declarations to make them more readable

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -23,12 +23,39 @@ public class ConsoleExercises {
 //        System.out.println(second);
 //        System.out.println(third);
 
-        String sentence;
-        Scanner sentenceIp = new Scanner(System.in);
+//        String sentence;
+//        Scanner sentenceIp = new Scanner(System.in);
+//
+//        System.out.println("Please enter a sentence - any sentence! -");
+//        sentence = sentenceIp.nextLine();
+//
+//        System.out.println("Your sentence was.... " + sentence);
 
-        System.out.println("Please enter a sentence - any sentence! -");
-        sentence = sentenceIp.next();
+        float width,
+                length,
+                area,
+                height,
+                volume,
+                perimeter;
 
-        System.out.println("Your sentence was.... " + sentence);
+        Scanner areaIp = new Scanner(System.in);
+
+        System.out.println("Please enter the width of the Codeup classroom: ");
+        width = areaIp.nextFloat();
+
+        System.out.println("Now enter the length of the classroom: ");
+        length = areaIp.nextFloat();
+
+        System.out.println("Now enter the height of the classroom: ");
+        height = areaIp.nextFloat();
+
+        area = length * width;
+        perimeter = (2 * length) + (2 * width);
+        volume = length * width * height;
+
+        System.out.println("The perimeter for the Codeup classroom is: " + perimeter);
+        System.out.println("The area for the Codeup classroom is: " + area);
+        System.out.println("The volume for the Codeup classroom is: " + volume);
+
     }
 }
