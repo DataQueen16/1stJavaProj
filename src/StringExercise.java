811273d37574e3140cb53cbadccec236d75f7ad3
Jennifer Walker <dataqueen16@gmail.com>
2018-05-01 14:57:49 -0500

    created one variable & printed out strings with escape characters to achieve the desired effect for double quotes, backslashes, etc.

---
+++ src/StringExercise.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class StringExercise {
+    public static void main(String[] args) {
+        Scanner output = new Scanner(System.in);
+
+        String message = "We don't need no education\nWe don't need no thought control";
+        System.out.println(message);
+
+        message = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(message);
+
+        message = "In windows, the main drive is usually C:\\\\";
+        System.out.println(message);
+
+        message = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(message);
+    }
+}
