e565acef7b396a909955ef2a05c4a7dc783769a4
Jennifer Walker <dataqueen16@gmail.com>
2018-05-01 09:59:22 -0500

    created table that outputs int, square, & cubed in a for loop

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 
@@ -10,16 +12,42 @@ public class ControlFlowExercises {
 //      For the multiples of five print “Buzz”.
 //      For numbers which are multiples of both three and five print “FizzBuzz”.
 
-        for (int i = 1; i < 101; i++) {
-            if ((i % 5 == 0) && (i % 3 == 0)){
-                System.out.println("FizzBuzz");
-            } else if (i % 5 == 0){
-                System.out.println("Buzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else {
-                System.out.println(i);
-            }
+//        for (int i = 1; i < 101; i++) {
+//            if ((i % 5 == 0) && (i % 3 == 0)){
+//                System.out.println("FizzBuzz");
+//            } else if (i % 5 == 0){
+//                System.out.println("Buzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+        int num;
+        int square;
+        int cube;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How high would you like to go?");
+        num = input.nextInt();
+
+        System.out.print("Integer");
+        System.out.printf("%10s", "| Squared");
+        System.out.printf("%8s", "| Cubed");
+        System.out.println();
+        System.out.println("------- | ------- | -----");
+
+
+        for (int i = 1; i <= num; i++){
+            System.out.printf("%-8s", i);
+            square = i * i;
+            System.out.printf("%-10s", "| " + square);
+            cube = i * i * i;
+            System.out.printf("%-8s", "| " + cube);
+            System.out.println();
+
         }
+
     }
 }
