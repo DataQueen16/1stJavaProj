69bb9a0cf10afeac63cc578bce75c81efab8369d
Jennifer Walker <dataqueen16@gmail.com>
2018-05-02 12:18:11 -0500

    added a subtract method then tested it. Also added a multiplication method but need to test. All methods have same convention so far

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -23,6 +23,7 @@ public class MethodsExercises {
 //        Create your multiplication method without the * operator (Hint: a loop might be helpful).
 //        Do the above with recursion.
 
+        // Add method test
         // Creating variables to hold the user responses from the keyboard
         int addAnswer1;
         int addAnswer2;
@@ -35,13 +36,43 @@ public class MethodsExercises {
         System.out.print("Enter the 2nd integer now: ");  // Prompt user for 2nd integer
         addAnswer2 = answer.nextInt();  // Store answer in addAnswer2
 
-        System.out.println("The result of " + addAnswer1 + " plus " + addAnswer2 + " is " + add(addAnswer1, addAnswer2));  // Call the add function using the user's answers
+        // Assign the result of the method call to a variable for the sout
+        int addTotal = add(addAnswer1, addAnswer2);
 
+        System.out.println("The result of " + addAnswer1 + " plus " + addAnswer2 + " is " + addTotal);  // Call the add function using the user's answers
+        System.out.println();
+
+        // Subtract method test
+        // Creating variables to hold user answers
+        double number1;
+        double number2;
+
+        // Prompt user for parameters
+        System.out.print("Let's subtract now.\nPlease enter a number with a decimal point: ");
+        number1 = answer.nextDouble();
+        System.out.print("Now enter another number with a decimal point: ");
+        number2 = answer.nextDouble();
+
+        // Assign the result of the method call to a variable for the sout
+        double subtractTotal = subtract(number1, number2);
+
+        System.out.println(number1 + " minus " + number2 + " gives you " + subtractTotal);
+        System.out.println();
 
     }
 
     public static int add(int num1, int num2) {
         int total = num1 + num2;
+        return total;
+    }
+
+    public static double subtract(double sub1, double sub2) {
+        double total = sub1 - sub2;
+        return total;
+    }
+
+    public static int multiply(int var1, int var2) {
+        int total = var1 * var2;
         return total;
     }
 }
