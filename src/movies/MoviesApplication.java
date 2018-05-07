f44607d3b9eb15d00d706c22dded86b6c151a6d7
Jennifer Walker <dataqueen16@gmail.com>
2018-05-07 12:31:30 -0500

    added an iterateCategory method for genre chosen

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -44,9 +44,62 @@ public class MoviesApplication {
 
     public String returnCategories() {
         input.getString("Greetings! What genre are you interested in?"); // Prompt user for genre
-        System.out.println("1. Drama\n2. Musical\n3. Sci-fi\n4. Horror\n\n5. Comedy\n6. Animated"); // List choices 1-6
 
-
-
+        int answer = input.getInt("1. Drama\n2. Musical\n3. Sci-fi\n4. Horror\n\n5. Comedy\n6. Animated");  // List choices 1-6
+        switch (answer){
+            case 1:
+                String input1 = "Drama";
+                for (Movie movie: movies){
+                    if (input1.equalsIgnoreCase(movie.getCategory())){
+                        System.out.println("Title: " + movie.getName() + ", Genre: " + movie.getCategory());
+                    }
+                }
+                break;
+            case 2:
+                String input2 = "Musical";
+                for (Movie movie: movies){
+                    if (input2.equalsIgnoreCase(movie.getCategory())){
+                        System.out.println("Title: " + movie.getName() + ", Genre: " + movie.getCategory());
+                    }
+                }
+                break;
+            case 3:
+                String input3 = "Scifi";
+                for (Movie movie: movies){
+                    if (input3.equalsIgnoreCase(movie.getCategory())){
+                        System.out.println("Title: " + movie.getName() + ", Genre: " + movie.getCategory());
+                    }
+                }
+                break;
+            case 4:
+                String input4 = "Horror";
+                for (Movie movie: movies){
+                    if (input4.equalsIgnoreCase(movie.getCategory())){
+                        System.out.println("Title: " + movie.getName() + ", Genre: " + movie.getCategory());
+                    }
+                }
+                break;
+            case 5:
+                String input5 = "Comedy";
+                for (Movie movie: movies){
+                    if (input5.equalsIgnoreCase(movie.getCategory())){
+                        System.out.println("Title: " + movie.getName() + ", Genre: " + movie.getCategory());
+                    }
+                }
+                break;
+            case 6:
+                String input6 = "Animated";
+                for (Movie movie: movies){
+                    if (input6.equalsIgnoreCase(movie.getCategory())){
+                        System.out.println("Title: " + movie.getName() + ", Genre: " + movie.getCategory());
+                    }
+                }
+                break;
+            default:
+                System.out.println("Incorrect number entered. Please try again.");
+                return returnCategories();
+        }
+        String end = "End of List";
+        return end;
     }
 }
