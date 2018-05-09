f61115701a89a986a706403d2c8c2fe250ffe0a9
Jennifer Walker <dataqueen16@gmail.com>
2018-05-09 11:23:56 -0500

    Square class extends Quadrilateral w all the methods, plus setting length & width as protected elements and assigning them for the square class

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -3,6 +3,11 @@ package shapes;
 class Rectangle extends Quadrilateral implements Measurable {
     double perimeter;
     double area;
+
+    public Rectangle(int length, int width) {
+        super(length, width);
+    }
+
     public double getPerimeter() {
         perimeter = (2 * length) + (2 * width);
         return perimeter;
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,8 +1,42 @@
 package shapes;
 
-class Square extends Rectangle {
+public class Square extends Quadrilateral {
+    protected double length;
+    protected double width;
 
+    public Square(double length, double width) {
+        super(length, width);
+    }
 
+    public double returnWidth(){
+        this.width = width;
+        return width;
+    }
+
+    public double returnLength(){
+        this.length = length;
+        return length;
+    }
+
+    @Override
+    public double setPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double setArea() {
+        return 0;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 0;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
 }
 
 
