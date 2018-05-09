d642e7b6ca95bb79f1c556da4ec4b3bd2f86c9c7
Jennifer Walker <dataqueen16@gmail.com>
2018-05-09 16:21:09 -0500

    changed getAllCards method to Card object. Need to change shuffle and comparison methods

--- src/CardGames/GameOfWar.java
+++ src/CardGames/GameOfWar.java
@@ -1,10 +1,21 @@
 package CardGames;
 
-public class GameOfWar {
-    String player1;
-    String player2;
+import java.util.Scanner;
 
-    GameOfWar war = new GameOfWar();
-    Player player = new Player();
-//    CardsArray cards = new CardsArray();
+public class GameOfWar {
+    Player play = new Player();
+    Scanner scanner = new Scanner(System.in);
+
+    public static String turn1;
+    public static String turn2;
+
+    public static int playCard1() {
+        turn1 = play.getRandom(1, 52);
+    }
+
+    public static int playCard2() {
+        turn2 = play.getRandom(1, 52);
+    }
+
 }
+
--- src/CardGames/Player.java
+++ src/CardGames/Player.java
@@ -1,10 +1,24 @@
 package CardGames;
 
+import java.util.Random;
+
 public class Player {
     int gamesWon;
-    String name;
+    String player1;
+    String player2;
 
     public Player(){
-        this.name = name;
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public int countMe(int gamesWon){
+        gamesWon += gamesWon;
+        return gamesWon;
+    }
+
+    public int getRandom(int min, int max) {
+        Random r = new Random();
+        return r.ints(min, (max + 1)).findFirst().getAsInt();
     }
 }
