bcc8565673fb441826f090ca82987659fdfa5e8b
Jennifer Walker <dataqueen16@gmail.com>
2018-05-10 16:48:22 -0500

    Added a Student class inside the grades package. Created an arraylist to hold grades for the student

--- src/CardGames/GameOfWar.java
+++ src/CardGames/GameOfWar.java
@@ -6,15 +6,5 @@ public class GameOfWar {
     Player play = new Player();
     Scanner scanner = new Scanner(System.in);
 
-    public static String turn1;
-    public static String turn2;
-
-    public static int playCard1() {
-
-    }
-
-    public static int playCard2() {
-    }
-
 }
 
