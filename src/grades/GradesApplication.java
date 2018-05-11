89b9e1b8d5fd80eca511be8d2f0c90bb98bec2c8
Jennifer Walker <dataqueen16@gmail.com>
2018-05-11 14:02:19 -0500

    Created 4 student objects w 3 grades each - got them into my hashmap. Now need to sout the values for the grades correctly

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -16,20 +16,33 @@ public class GradesApplication {
         Julius.addGrade(92.89);
         Julius.addGrade(89.8);
 
+        System.out.println(Julius);
+
         Jennifer.addGrade(99.54);
         Jennifer.addGrade(92.34);
         Jennifer.addGrade(97.64);
+
+        System.out.println(Jennifer);
 
         Jackie.addGrade(69.54);
         Jackie.addGrade(75.87);
         Jackie.addGrade(89.54);
 
+        System.out.println(Jackie);
+
         Stella.addGrade(89.54);
         Stella.addGrade(93.56);
         Stella.addGrade(92.25);
 
+        System.out.println(Stella);
 
-        HashMap<String, List<Double>> students = new HashMap<String,List<Double>>();
+
+        HashMap<String, Student> students = new HashMap<>();
+
+        students.put(Julius.getName(), Julius);
+        students.put(Jennifer.getName(), Jennifer);
+        students.put(Jackie.getName(), Jackie);
+        students.put(Stella.getName(), Stella);
 
 
         System.out.println(students);
