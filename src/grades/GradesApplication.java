519711a3e685ecab6e27860b64b795c9085ab662
Jennifer Walker <dataqueen16@gmail.com>
2018-05-11 12:31:13 -0500

    Added 4 student objects with 3 grades each in the GradesApplication class

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -7,7 +7,28 @@ import java.util.List;
 public class GradesApplication {
     public static void main(String[] args) {
 
-        Student Julius = new Student();
+        Student Julius = new Student("LovecraftNow");
+        Student Jennifer = new Student("DataQueen16");
+        Student Jackie = new Student("DegrassiJrHigh");
+        Student Stella = new Student("NewMexicoMama");
+
+        Julius.addGrade(98.0);
+        Julius.addGrade(92.89);
+        Julius.addGrade(89.8);
+
+        Jennifer.addGrade(99.54);
+        Jennifer.addGrade(92.34);
+        Jennifer.addGrade(97.64);
+
+        Jackie.addGrade(69.54);
+        Jackie.addGrade(75.87);
+        Jackie.addGrade(89.54);
+
+        Stella.addGrade(89.54);
+        Stella.addGrade(93.56);
+        Stella.addGrade(92.25);
+
+
         HashMap<String, List<Double>> students = new HashMap<String,List<Double>>();
 
 
