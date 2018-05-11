abf21a4561b07aad3339918fd273c7624d4e9d0f
Jennifer Walker <dataqueen16@gmail.com>
2018-05-11 10:52:01 -0500

    created getGradesAverage method, tested adding items to arraylist and the average method - works!

--- src/grades/Student.java
+++ src/grades/Student.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 public class Student {
     private String name;
-    private ArrayList<Integer> grades;
+    private ArrayList<Double> grades = new ArrayList<>();
 
-    public Student(String name, ArrayList grades) {
+    public Student(String name, double grades) {
         this.name = name;
-        grades = new ArrayList();
+        this.grades.add(grades);
     }
 
     // returns the student's name
@@ -16,12 +16,38 @@ public class Student {
         return name;
     }
     // adds the given grade to the grades property
-    public void addGrade(int grade) {
+    public void addGrade(double grade) {
         grades.add(grade);
     }
     // returns the average of the students grades
     public double getGradeAverage() {
-        return name;
+        if (grades.isEmpty()) {
+            System.out.println("I'm sorry, this student has no grades");
+            return 0;
+        } else {
+
+            double totalScore = 0;
+
+            for (int i = 0; i < grades.size(); i++) {
+                double score = grades.get(i);
+                totalScore += score;
+                System.out.println(totalScore);
+            }
+
+            double total = totalScore / grades.size();
+            return total;
+        }
+    }
+
+    public static void main(String[] args) {
+        Student Michael = new Student("Michael", 75.23);
+        Michael.addGrade(89.53);
+        Michael.addGrade(75);
+        Michael.addGrade(88.75);
+        Michael.addGrade(69.90);
+
+        System.out.println(Michael.grades);
+        System.out.println(Michael.getGradeAverage());
     }
 
 }
